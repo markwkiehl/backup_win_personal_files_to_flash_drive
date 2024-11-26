3aa326a2f2dec605e1e69a7d74e57af842f3cb13
Mark W Kiehl <63211142+markwkiehl@users.noreply.github.com>
2024-11-25 21:41:05 -0500

    Update backup_win_personal_files_to_flash_drive.bat

--- backup_win_personal_files_to_flash_drive.bat
+++ backup_win_personal_files_to_flash_drive.bat
@@ -18,20 +18,20 @@ echo Only new or modified files will be copied.
 echo The detailed copy operations will not be shown (please be patient).
 echo When the backup is complete, make sure to properly eject the drive before you disconnect it!
 echo.
-echo 1 of 7   "C:\Users\Mark Kiehl\Documents"
-robocopy "C:\Users\Mark Kiehl\Documents" %~d0\active\Documents /E /DCOPY:DA /COPY:DAT /R:0 /W:0 /B /NP /NFL /NDL /NJH
-echo 2 of 7   "C:\Users\Mark Kiehl\Pictures"
-robocopy "C:\Users\Mark Kiehl\Pictures" %~dp0active\Pictures /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
-echo 3 of 7   "C:\Users\Mark Kiehl\Videos"
-robocopy "C:\Users\Mark Kiehl\Videos" %~dp0active\Videos /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
-echo 4 of 7   "C:\Users\Mark Kiehl\Music"
-robocopy "C:\Users\Mark Kiehl\Music" %~dp0active\Music /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
-echo 5 of 7   "C:\Users\Mark Kiehl\Desktop"
-robocopy "C:\Users\Mark Kiehl\Desktop" %~dp0active\Desktop /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
-echo 6 of 7   "C:\Users\Mark Kiehl\Downloads"
-robocopy "C:\Users\Mark Kiehl\Downloads" %~dp0active\Downloads /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
-echo 7 of 7   "C:\Users\Mark Kiehl\Favorites"
-robocopy "C:\Users\Mark Kiehl\Favorites" %~dp0active\Favorites /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
+echo 1 of 7   "%HOMEPATH%\Documents"
+robocopy "%HOMEPATH%\Documents" %~d0\active\Documents /E /DCOPY:DA /COPY:DAT /R:0 /W:0 /B /NP /NFL /NDL /NJH
+echo 2 of 7   "%HOMEPATH%\Pictures"
+robocopy "%HOMEPATH%\Pictures" %~dp0active\Pictures /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
+echo 3 of 7   "%HOMEPATH%\Videos"
+robocopy "%HOMEPATH%\Videos" %~dp0active\Videos /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
+echo 4 of 7   "%HOMEPATH%\Music"
+robocopy "%HOMEPATH%\Music" %~dp0active\Music /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
+echo 5 of 7   "%HOMEPATH%\Desktop"
+robocopy "%HOMEPATH%\Desktop" %~dp0active\Desktop /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
+echo 6 of 7   "%HOMEPATH%\Downloads"
+robocopy "%HOMEPATH%\Downloads" %~dp0active\Downloads /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
+echo 7 of 7   "%HOMEPATH%\Favorites"
+robocopy "%HOMEPATH%\Favorites" %~dp0active\Favorites /E /DCOPY:DA /COPY:DAT /R:2 /W:3 /B /NP /NFL /NDL /NJH
 ENDLOCAL
 echo Backup complete
 pause
